9f80296f1c549deeab153259efb5e648b65288bc
Fhriii <muhammadfahrrri@gmail.com>
2024-08-29 14:54:25 +0700

    login

--- cobachart/Properties/Settings.Designer.cs
+++ cobachart/Properties/Settings.Designer.cs
@@ -8,22 +8,29 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace cobachart.Properties
-{
-
-
+namespace cobachart.Properties {
+    
+    
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "11.0.0.0")]
-    internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase
-    {
-
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "17.11.0.0")]
+    internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
+        
         private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings())));
-
-        public static Settings Default
-        {
-            get
-            {
+        
+        public static Settings Default {
+            get {
                 return defaultInstance;
+            }
+        }
+        
+        [global::System.Configuration.ApplicationScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.SpecialSettingAttribute(global::System.Configuration.SpecialSetting.ConnectionString)]
+        [global::System.Configuration.DefaultSettingValueAttribute("Data Source=MYPCPRO\\SQLEXPRESS;Initial Catalog=testfahri;Integrated Security=True" +
+            ";TrustServerCertificate=True")]
+        public string testfahriConnectionString {
+            get {
+                return ((string)(this["testfahriConnectionString"]));
             }
         }
     }
---
+++ cobachart/login.xaml.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Linq;
+using System.Windows;
+// Ganti dengan namespace yang sesuai dengan file .dbml Anda
+
+namespace cobachart
+{
+    /// <summary>
+    /// Interaction logic for login.xaml
+    /// </summary>
+    public partial class login : Window
+    {
+        public login()
+        {
+            InitializeComponent();
+        }
+
+        private void Login_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                // Buat instance konteks LINQ to SQL
+                using (var db = new DataClasses1DataContext()) // Ganti dengan nama konteks LINQ to SQL Anda
+                {
+                    // Ambil username dari textbox
+                    string userValue = userText.Text;
+
+                    // Cari pengguna dengan username tersebut
+                    var user = db.users.SingleOrDefault(u => u.username == userValue); // Ganti 'Users' dengan nama tabel di .dbml Anda
+
+                    if (user != null)
+                    {
+                        // Bandingkan password
+                        string passValue = passText.Text;
+                        if (user.password == passValue) // Ini hanya jika password disimpan sebagai teks biasa
+                        {
+                            this.Hide();
+                            var main = new MainWindow();
+                            main.ShowDialog();
+                        }
+                        else
+                        {
+                            MessageBox.Show("Password Salah");
+                        }
+                    }
+                    else
+                    {
+                        MessageBox.Show("Username Salah");
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Terjadi kesalahan: {ex.Message}");
+            }
+        }
+    }
+}
